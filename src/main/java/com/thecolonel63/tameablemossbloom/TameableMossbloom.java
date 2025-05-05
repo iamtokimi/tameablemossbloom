942f813c8c3e21b9e93ad54fbaffd52d40b52f6b
Tokimi <tokimitiktok@gmail.com>
2025-05-05 10:26:04 -0700

    Create TameableMossbloom.java

---
+++ src/main/java/com/thecolonel63/tameablemossbloom/TameableMossbloom.java
@@ -0,0 +1,12 @@
+package com.thecolonel63.tameablemossbloom;
+
+import com.mojang.logging.LogUtils;
+import net.fabricmc.api.ModInitializer;
+
+public class TameableMossbloom implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        LogUtils.getLogger().info("TameableMossbloom by thecolonel63 initialized!");
+    }
+}
